95fa506d4319b21dcaf10f7eb467b01cfa5ebb00
martijnalexion <58816160+martijnalexion@users.noreply.github.com>
2019-12-12 15:32:03 +0100

    Update setup.py
    Update setuptools to newest version

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
-from distutils.core import setup
+# -*- coding: utf-8 -*-
+# from pip.req import parse_requirements
+from setuptools import setup
+from setuptools import find_packages
 
-classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Environment :: Win32 (MS Windows)',
-  'Intended Audience :: Developers',
-  'Intended Audience :: System Administrators',
-  'License :: PSF',
-  'Natural Language :: English',
-  'Operating System :: Microsoft :: Windows :: Windows 95/98/2000',
-  'Topic :: System :: Systems Administration'
-]
 
 setup (
   name = "events",
@@ -19,6 +12,16 @@ setup (
   author_email = "tim.golden@iname.com",
   url = "http://tgolden.sc.sabren.com/python/events.html",
   license = "http://www.python.org/psf/license.html",
-  py_modules = ["events"]
+  packages = ["events"],
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Win32 (MS Windows)',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: PSF',
+    'Natural Language :: English',
+    'Operating System :: Microsoft :: Windows :: Windows 95/98/2000',
+    'Topic :: System :: Systems Administration'
+  ]
 )
 
